0e916c7b28b378d9c36dc98b83f03fb72c88237a
suryak-10 <ayrus4444@gmail.com>
2024-11-26 06:23:38 +0000

    socket client implemented

--- app/socket/[roomId]/page.tsx
+++ app/socket/[roomId]/page.tsx
@@ -3,9 +3,10 @@ import { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
 const socket = io('http://localhost:1000');
-const roomId = '63927472' + Math.ceil(Math.random() * 100);
+// const roomId = '63927472' + Math.ceil(Math.random() * 100);
 
-const Home = () => {
+const Home = ({ params }: { params: { roomId: string } }) => {
+  const roomId = params.roomId;
   const [message, setMessage] = useState('');
   const [receivedMessage, setReceivedMessage] = useState('');
 
@@ -26,6 +27,7 @@ const Home = () => {
     <div>
       <h1>Socket.IO with Next.js</h1>
       <p>Room {roomId}</p>
+      <p>Status {socket.connected ? "connected": 'disconnected'}</p>
       <button
         onClick={() => {
           socket.emit('joinRoom', { roomId, type: 'client', name: 'surya' })
@@ -33,11 +35,13 @@ const Home = () => {
         }}
       >send</button>
 
-      {['fileCompleted', 'fileStarted', 'fileStarted', 'getRoom'].map((fileEvenv, index) => <button key={index}
+      {['fileCompleted', 'fileStarted', 'fileErrored', 'getRoom'].map((fileEvenv, index) => <button key={index}
         onClick={() => {
           socket.emit(fileEvenv, { roomId })
         }}
       >{fileEvenv}</button>)}
+
+      <button onClick={() => socket.disconnect()}>disconnect</button>
 
       <div>
         <h2>Received Message:</h2>
